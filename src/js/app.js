8cceecd52a5c2919c14be9476d5c5d82cfbfc500
Nastya <shamr44@mail.ru>
2023-08-12 01:35:24 +0300

    feat: add basic layout and basic styles for all sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as favoritesFilter from "./modules/favoritesFilter.js";
+import * as hamburger from "./modules/hamburger.js";
+
 
 flsFunctions.isWebp();
 favoritesFilter.filter();
+hamburger.toggleHamburger();
