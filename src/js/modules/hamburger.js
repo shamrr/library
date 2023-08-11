13c8ad105bbd4e4741dcddfa0136babb297dd7a4
Nastya <shamr44@mail.ru>
2023-08-12 01:33:08 +0300

    feat: add function for hamburger

---
+++ src/js/modules/hamburger.js
@@ -0,0 +1,40 @@
+const navbar = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+const navLinks = document.querySelectorAll('.navbar__link'); 
+
+export const toggleHamburger = () => {
+    sidebar.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navbar.classList.contains('active')) {
+                closeNavbar();
+            }
+        });
+    });
+    // if (navbar.classList.contains('active')) {
+    //     closeWhenClickedOutsideOfElement();
+    // }
+    
+}
+
+function closeNavbar() {
+    navbar.classList.remove('active');
+    hamburger.classList.remove('active');
+}
+
+// function closeWhenClickedOutsideOfElement() {
+//     document.addEventListener('click', (e) => {
+//         const withinBoundaries = e.composedPath().includes(navbar);
+
+//         if (!withinBoundaries) {
+//             closeNavbar();
+//         }
+//     });
+// }
+
+
