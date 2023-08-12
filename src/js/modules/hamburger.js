6ea4d2d5749fecf72c7e88e3efe8ee8cc12fdf02
Nastya <shamr44@mail.ru>
2023-08-12 16:23:43 +0300

    feat: add soft opening menu

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector('.navbar');
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
 const navLinks = document.querySelectorAll('.navbar__link'); 
+const icons = document.querySelector('.icons');
 
 export const toggleHamburger = () => {
     sidebar.addEventListener('click', () => {
